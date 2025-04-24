d0a1adf0cca3ad831597bd198ddc68ce30db77d5
Lucas Remígio <lucasxavier5435@hotmail.com>
2025-04-24 19:42:12 +0100

    fix: agora sim vai pintar

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
-	// Define environment variables with fallbacks
-	define: {
-		'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
-			process.env.VITE_BACKEND_URL || 'http://localhost:8080'
-		),
-		'import.meta.env.VITE_SOCKETS_URL': JSON.stringify(
-			process.env.VITE_SOCKETS_URL || 'ws://localhost:8090'
-		)
-	}
 });
