df08b3f14571a2e4df43df4466d350850bf2836a
Lucas Remígio <lucasxavier5435@hotmail.com>
2025-05-05 18:54:38 +0100

    add: folder de data e fix da foreign key

--- backend/cmd/migrate/migrations/20250209150903_alter-category-key.up.sql
+++ backend/cmd/migrate/migrations/20250209150903_alter-category-key.up.sql
@@ -2,4 +2,4 @@ ALTER TABLE categories
   DROP FOREIGN KEY categories_ibfk_1,
   DROP INDEX user_category,
   ADD UNIQUE KEY user_category (user_id, transaction_type_id, category_name),
-  ADD CONSTRAINT categories_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
+  ADD CONSTRAINT categories_ibfk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
